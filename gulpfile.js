b8db535dc78ec8adf283a37ef0674d643f9f624c
ikerlavan <ikerlavan@gmail.com>
2017-04-19 20:03:45 +0200

    Primera prueba

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var less = require('gulp-less');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 var header = require('gulp-header');
@@ -11,7 +12,7 @@ var pkg = require('./package.json');
 var banner = ['/*!\n',
     ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
     ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
-    ' * Licensed under <%= pkg.license %> (https://github.com/BlackrockDigital/<%= pkg.name %>/blob/master/LICENSE)\n',
+    ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n',
     ' */\n',
     ''
 ].join('');
@@ -38,7 +39,7 @@ gulp.task('minify-css', ['sass'], function() {
         }))
 });
 
-// Minify custom JS
+// Minify JS
 gulp.task('minify-js', function() {
     return gulp.src('js/new-age.js')
         .pipe(uglify())
@@ -50,23 +51,19 @@ gulp.task('minify-js', function() {
         }))
 });
 
-// Copy vendor files from /node_modules into /vendor
-// NOTE: requires `npm install` before running!
+// Copy lib libraries from /node_modules into /lib
 gulp.task('copy', function() {
     gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
-        .pipe(gulp.dest('vendor/bootstrap'))
+        .pipe(gulp.dest('lib/bootstrap'))
 
     gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-        .pipe(gulp.dest('vendor/jquery'))
+        .pipe(gulp.dest('lib/jquery'))
 
     gulp.src(['node_modules/tether/dist/js/*.js'])
-        .pipe(gulp.dest('vendor/tether'))
-
-    gulp.src(['node_modules/jquery.easing/*.js'])
-        .pipe(gulp.dest('vendor/jquery-easing'))
+        .pipe(gulp.dest('lib/tether'))
 
     gulp.src(['node_modules/simple-line-icons/*/*'])
-        .pipe(gulp.dest('vendor/simple-line-icons'))
+        .pipe(gulp.dest('lib/simple-line-icons'))
 
 
     gulp.src([
@@ -77,10 +74,10 @@ gulp.task('copy', function() {
             '!node_modules/font-awesome/*.md',
             '!node_modules/font-awesome/*.json'
         ])
-        .pipe(gulp.dest('vendor/font-awesome'))
+        .pipe(gulp.dest('lib/font-awesome'))
 })
 
-// Default task
+// Run everything
 gulp.task('default', ['sass', 'minify-css', 'minify-js', 'copy']);
 
 // Configure the browserSync task
@@ -100,4 +97,15 @@ gulp.task('dev', ['browserSync', 'sass', 'minify-css', 'minify-js'], function() 
     // Reloads the browser whenever HTML or JS files change
     gulp.watch('*.html', browserSync.reload);
     gulp.watch('js/**/*.js', browserSync.reload);
+});
+
+// Compile LESS files from /less into /css
+gulp.task('less', function() {
+    return gulp.src('less/new-age.less')
+        .pipe(less())
+        .pipe(header(banner, { pkg: pkg }))
+        .pipe(gulp.dest('css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
 });
--- js/new-age.js
+++ js/new-age.js
@@ -1,32 +1,27 @@
 (function($) {
     "use strict"; // Start of use strict
 
-    // Smooth scrolling using jQuery easing
-    $('a[href*="#"]:not([href="#"])').click(function() {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                $('html, body').animate({
-                    scrollTop: (target.offset().top - 48)
-                }, 1000, "easeInOutExpo");
-                return false;
-            }
-        }
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $(document).on('click', 'a.page-scroll', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 54)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
     });
 
-    // Activate scrollspy to add active class to navbar items on scroll
+    // Highlight the top nav as scrolling occurs
     $('body').scrollspy({
         target: '#mainNav',
         offset: 54
     });
 
-    // Closes responsive menu when a link is clicked
+    // Closes the Responsive Menu on Menu Item Click
     $('.navbar-collapse>ul>li>a').click(function() {
         $('.navbar-collapse').collapse('hide');
     });
 
-    // Collapse the navbar when page is scrolled
+    // jQuery to collapse the navbar on scroll
     $(window).scroll(function() {
         if ($("#mainNav").offset().top > 100) {
             $("#mainNav").addClass("navbar-shrink");
